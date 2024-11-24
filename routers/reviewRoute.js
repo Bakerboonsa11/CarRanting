e4ec3ee24e5dfdb6d0fb7262e1f12694fe9aea7a
bonsa baker <bakerboonsa@gmail.com>
2024-11-24 21:30:03 +0300

    all functions of the review

--- routers/reviewRoute.js
+++ routers/reviewRoute.js
@@ -2,12 +2,14 @@ const express=require("express")
 const reviewController=require('./../controllers/reviewCOntroller')
 const authController=require('./../controllers/authController')
 const Router=express.Router({mergeParams:true})
-
+Router.use(authController.protect)
 Router.route('/')
-.post(authController.protect,authController.strictTo("user"),reviewController.createReviewForCar)
+.post(authController.strictTo("user"),authController.strictTo("user"),reviewController.createReviewForCar)
 .get(reviewController.getAllReview)
 
 
 Router.route('/:id')
-.delete(reviewController.deleteReview)
+.delete(authController.strictTo("admin","user"),reviewController.deleteReview)
+.patch(authController.strictTo("admin","user"),reviewController.updateReview)
+.get(authController.strictTo("admin","user"),reviewController.getReview)
 module.exports=Router
