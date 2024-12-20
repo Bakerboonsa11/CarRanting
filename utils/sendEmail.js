5d5ea06164718077d0ebf4e3605cdd73cd12a9fc
bonsa baker <bakerboonsa@gmail.com>
2024-12-20 11:34:20 +0300

    secure

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -43,6 +43,9 @@ module.exports=class Email {
     if(process.env.NODENV==="production"){
    return  nodemailer.createTransport({
    service: 'gmail',
+   host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
    auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAILPASSWORD,  // Your App Password (not your main password)
